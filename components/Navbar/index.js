9bb7e6a2a66ed1f03566dbb655606ec3d90e2b4a
mohamed-lababidi <lababidi.mohamed@but.fr>
2021-06-19 22:53:18 +0200

    Work In Progress

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,32 @@
 const Navbar = () => {
     return (
-        <div>
-            <h1>hello world</h1>
+        <>
+            <Nav>
+                <NavLink href="/">
+                    <h1>Logo</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
 
-        </div>
+                    <NavLink href="/about" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink href="/services" activeStyle>
+                        Services
+                    </NavLink>
+                    <NavLink href="/contact-us" activeStyle>
+                        Contact Us
+                    </NavLink>
+                    <NavLink href="/sign-up" activeStyle>
+                        Sign Up
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink href="/signin">Sign In</NavBtnLink>
+                </NavBtn>
+            </Nav>
+
+        </>
     )
 }
 
